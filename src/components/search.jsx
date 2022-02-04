73f0d800e16e61477dc6bb67e3aa1ddde24172e7
snjtsam <sanjeetsam07@gmail.com>
2022-02-04 23:09:16 +0530

    start

---
+++ src/components/search.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+class Search extends React.Component {
+  render() {
+    return (
+      <>
+        <input placeholder="Search Products"></input>
+        <button>Search</button>
+      </>
+    );
+  }
+}
+export default Search;
